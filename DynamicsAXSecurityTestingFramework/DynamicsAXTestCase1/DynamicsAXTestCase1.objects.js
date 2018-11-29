9baf6b1b26ae79ddc746ae2c4dc9c448d8598309
dmarkovtsev <denis@inflectra.com>
2018-11-29 15:09:38 +0300

    TestCase1 sample implementation

--- DynamicsAXSecurityTestingFramework/DynamicsAXTestCase1/DynamicsAXTestCase1.objects.js
+++ DynamicsAXSecurityTestingFramework/DynamicsAXTestCase1/DynamicsAXTestCase1.objects.js
@@ -1,5 +1,5 @@
 var saved_script_objects={
-	"TabControl": {
+	"AddressBarContainer": {
 		"locations": [
 			{
 				"locator_name": "UIAutomation",
@@ -13,17 +13,16 @@ var saved_script_objects={
 		"window_class": "AxMainFrame",
 		"object_text": "",
 		"object_role": "param:object_class",
-		"object_class": "Tab",
+		"object_class": "Pane",
 		"version": 0,
-		"object_type": "UIATabControl",
-		"object_flavor": "TabControl",
-		"object_name": "TabControl",
-		"ignore_object_name": true,
-		"object_library": "UIAutomation",
+		"object_type": "DynamicsAXAddressBar",
+		"object_flavor": "Edit",
+		"object_name": "AddressBarContainer",
+		"object_library": "DynamicsAX",
 		"window_name": "regex:.Microsoft Dynamics AX.*",
-		"location": "navPaneTable/Pane/Tab"
+		"location": "Pane/TopRow/AddressBarContainer"
 	},
-	"Tree": {
+	"Maintain": {
 		"locations": [
 			{
 				"locator_name": "UIAutomation",
@@ -36,15 +35,107 @@ var saved_script_objects={
 		],
 		"window_class": "AxMainFrame",
 		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Group",
+		"version": 0,
+		"object_type": "DynamicsAXMenu",
+		"object_flavor": "Menu",
+		"object_name": "Maintain",
+		"object_library": "DynamicsAX",
+		"window_name": "regex:.Microsoft Dynamics AX.*",
+		"location": "Pane/Pane/AxContainerControl/ActionPaneTab/btnGrpMaintain"
+	},
+	"ButtonGroupUsersRoles": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "",
+					"window_class": "regex:(WindowsForms10.)?Window.\\d+.app[a-z0-9_\\.]+"
+				}
+			}
+		],
+		"window_class": "AxTopLevelFrame",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Group",
+		"version": 0,
+		"object_type": "DynamicsAXMenu",
+		"object_flavor": "Menu",
+		"object_name": "ButtonGroupUsersRoles",
+		"object_library": "DynamicsAX",
+		"window_name": "regex:.User.*",
+		"location": "AxContainerControl/ActionPaneTabUsersRoles/ButtonGroupUsersRoles"
+	},
+	"Grant_access_to_all_organization": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "AxChildFrame"
+				}
+			}
+		],
+		"window_class": "AxTopLevelFrame",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "RadioButton",
+		"version": 0,
+		"object_type": "UIARadioButton",
+		"object_flavor": "Radio",
+		"object_name": "Grant access to all organizations",
+		"object_library": "UIAutomation",
+		"window_name": "regex:.Organizations for the user.*",
+		"location": "regex:Organizations for the user.*/SysSecRoleAssignOM/Tab/Pane/Select the organizations for this user and role/Pane/Grant access to all organizations"
+	},
+	"Close": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "",
+					"window_class": "regex:HwndWrapper\\[Ax32\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "AxTopLevelFrame",
+		"object_text": "",
 		"object_role": "param:object_flavor",
 		"object_class": "param:object_flavor",
 		"version": 0,
-		"object_type": "UIATree",
-		"object_flavor": "Tree",
-		"object_name": "Tree",
+		"object_type": "UIAButton",
+		"object_flavor": "Button",
+		"object_name": "Close",
 		"object_library": "UIAutomation",
-		"window_name": "regex:.Microsoft Dynamics AX.*",
-		"location": "navPaneTable/Pane/Tab/Home/Tree"
+		"window_name": "regex:.Organizations for the user.*",
+		"location": "Custom/closeItem/CloseButton"
+	},
+	"Close1": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "",
+					"window_class": "regex:HwndWrapper\\[Ax32\\.exe.+\\]"
+				}
+			}
+		],
+		"window_class": "AxTopLevelFrame",
+		"object_text": "",
+		"object_role": "param:object_class",
+		"object_class": "Text",
+		"version": 0,
+		"object_type": "UIALabel",
+		"object_flavor": "Label",
+		"object_name": "Close",
+		"object_library": "UIAutomation",
+		"window_name": "regex:.User.*",
+		"location": "Custom/closeItem/CloseButton/Close"
 	},
 	"Grid": {
 		"locations": [
@@ -52,7 +143,7 @@ var saved_script_objects={
 				"locator_name": "UIAutomation",
 				"location": {
 					"location": "param:location",
-					"window_name": "regex:.Global address book.*",
+					"window_name": "regex:.Users.*",
 					"window_class": "AxChildFrame"
 				}
 			}
@@ -67,6 +158,30 @@ var saved_script_objects={
 		"object_name": "Grid",
 		"object_library": "DynamicsAX",
 		"window_name": "regex:.Microsoft Dynamics AX.*",
-		"location": "regex:Global address book.*/GlobalAddressBookListPage/Grid"
+		"location": "regex:Users.*/SysUserInfoPage/Grid"
+	},
+	"Tree": {
+		"locations": [
+			{
+				"locator_name": "UIAutomation",
+				"location": {
+					"location": "param:location",
+					"window_name": "param:window_name",
+					"window_class": "AxChildFrame"
+				}
+			}
+		],
+		"window_class": "AxTopLevelFrame",
+		"object_text": "",
+		"object_role": "param:object_flavor",
+		"object_class": "param:object_flavor",
+		"version": 0,
+		"object_type": "UIATree",
+		"object_flavor": "Tree",
+		"object_name": "Tree",
+		"ignore_object_name": true,
+		"object_library": "UIAutomation",
+		"window_name": "regex:.User.*",
+		"location": "regex:User.*/SysUserInfoDetail/Tab/User details/regex:.*[1]/Pane/User's roles/Tree"
 	}
 };
